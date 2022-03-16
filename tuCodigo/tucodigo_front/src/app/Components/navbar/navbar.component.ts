648c21c14de52a92866f1917a2a4a3bb020e5fc0
Ricardo Zarate Varela <zaratevarelaricardo@gmail.com>
2022-03-16 11:11:37 -0500

    corregido librerias y modules

--- tuCodigo/tucodigo_front/src/app/Components/navbar/navbar.component.ts
+++ tuCodigo/tucodigo_front/src/app/Components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import jwtDecode from 'jwt-decode';
+import { UserService } from 'src/app/Services/user.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
   }
