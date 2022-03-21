007bbbd8ad589d380edd5bdd169350efaabaa917
Daniel-Garcia-g-ia <daniel.g.1393@gmail.com>
2022-03-21 16:49:58 -0500

    [add] add Back courses

--- tuCodigo/tucodigo_back/src/routes/courses.routes.js
+++ tuCodigo/tucodigo_back/src/routes/courses.routes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const coursesControllers = require("../controllers/courses.controllers");
 
-router.get("/get-course", coursesControllers.getCourses);
-router.get("/get-one-course", coursesControllers.getOneCourses);
-router.post("/create-course", coursesControllers.createCourse);
-router.delete("/delete-course/:courseId", coursesControllers.deleteCourses);
-router.put("/update-course/:courseId", coursesControllers.updateCourses);
+router.get("/get-curses", coursesControllers.getCourses);
+router.get("/get-curse/:numCurse", coursesControllers.getOneCourses);
+router.post("/create-curse", coursesControllers.createCourse);
+/* router.delete("/delete-course/", coursesControllers.deleteCourses); */
+/* router.put("/update-course/:courseId", coursesControllers.updateCourses); */
 
 module.exports = router;
--- tuCodigo/tucodigo_front/src/app/Services/couses.service.ts
+++ tuCodigo/tucodigo_front/src/app/Services/couses.service.ts
@@ -3,23 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Curses } from 'src/app/Models/courses.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CourseService {
-  selectCurses:Curses
+  selectCurses: Curses;
 
-  constructor(public http : HttpClient) {
-    this.selectCurses= new Curses
-   }
-
-  getEjercise (){
-    return this.http.get('http://localhost:5000/api/products/get-curses')
-  }
-  getOneEjercice (num:number){
-    return this.http.get(`http://localhost:5000/api/products/get-curse/${num}`)
+  constructor(public http: HttpClient) {
+    this.selectCurses = new Curses();
   }
 
-
-
-
+  getEjercise() {
+    /*   return this.http.get('http://localhost:3000/api/products/get-curses') */
+    return this.http.get('http://localhost:3000/api/courses/get-curses');
+  }
+  getOneEjercice(num: number) {
+    /* return this.http.get(`http://localhost:3000/api/products/get-curse/${num}`) */
+    return this.http.get(`http://localhost:3000/api/courses/get-curse/${num}`);
+  }
 }
