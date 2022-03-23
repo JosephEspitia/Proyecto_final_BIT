39dbae45d957a332e11c042b1cca1c8275af453c
JosephEspitia <josephespitiac@gmail.com>
2022-03-22 20:13:41 -0500

    fewchance

--- tuCodigo/tucodigo_front/src/app/Components/java-script-course/java-script-course.component.ts
+++ tuCodigo/tucodigo_front/src/app/Components/java-script-course/java-script-course.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CourseService } from 'src/app/Services/couses.service';
 @Component({
   selector: 'app-java-script-course',
   templateUrl: './java-script-course.component.html',
   styleUrls: ['./java-script-course.component.css']
 })
 export class JavaScriptCourseComponent implements OnInit {
+  link = 0;
+  cursoActive = false;
+  numeroCurso = 0;
 
-  constructor() { }
+  constructor(public courseService: CourseService) { }
 
   ngOnInit(): void {
+  }
+  CursoActive() {
+    this.cursoActive = true;
+    this.numeroCurso = 1;
+    return this.cursoActive;
   }
 
 }
